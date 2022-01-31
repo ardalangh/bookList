9fc6c1383b27e8e49c8fca90e8dd4b9abbc12bd0
ardalangh <rdghoorchian@gmail.com>
2022-01-31 15:53:30 -0800

    ALLOWED_HOSTS added to setting

--- bookList/settings.py
+++ bookList/settings.py
@@ -146,3 +146,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
